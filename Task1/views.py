fd81096b878ca39564d23b76249fc8e40bb2b10f
Udit26085 <bhatiaudit55@gmail.com>
2025-05-04 23:05:12 +0530

    Updated post method

--- Task1/views.py
+++ Task1/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from .models import Member
 from .forms import MemberForm
 
 # Create your views here.
+
+context = {
+        "all_members": Member.objects.all
+    }
+
 def show(request):
     """
     To show all the values in the database
     """
-    context = {
-        "all_members": Member.objects.all
-    }
     return render(request, 'show.html', context)
 
 
@@ -18,10 +20,10 @@ def join(request):
     To add values in the database
     """
     if request.method == "POST":
-        form = MemberForm(request.post or None)
+        form = MemberForm(request.POST or None)
         if form.is_valid():
             form.save()
-        return render(request, 'join.html')
+        return render(request, 'show.html', context)
     else:
-        return render(request, 'join.html')
+        return render(request, "join.html")
 
