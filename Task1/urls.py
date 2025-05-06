7d8525c81ed397a5cb9198b4026c1fa8087d6ec6
Udit26085 <bhatiaudit55@gmail.com>
2025-05-06 17:16:21 +0530

    Updated endpoints

--- Task1/urls.py
+++ Task1/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from Task1 import views
 
 urlpatterns = [
-    path("join", views.join, name='join'),
-    path("", views.join, name='home'),
+    path("join", views.join, name="join"),
     path("show", views.show, name="show")
 ]
 
